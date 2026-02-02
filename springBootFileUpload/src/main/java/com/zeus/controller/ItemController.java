f02191c0844ada4fa8b63ae2bdb4445ed1f2ebd1
Ray <dnl1992@naver.com>
2026-02-02 10:53:04 +0900

    [생성] springBootFileUpload 프로젝트 환경 생성

---
+++ springBootFileUpload/src/main/java/com/zeus/controller/ItemController.java
@@ -0,0 +1,30 @@
+package com.zeus.controller;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zeus.service.ItemService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@MapperScan(basePackages = "com.zeus.mapper")
+@RequestMapping("/item")
+public class ItemController {
+	
+	@Autowired
+	private ItemService itemService;
+	
+	@GetMapping("/createForm")
+	public String itemCreateForm(Model model) {
+		log.info("/createForm");
+		return "item/createFrom";
+	}
+	
+
+}
